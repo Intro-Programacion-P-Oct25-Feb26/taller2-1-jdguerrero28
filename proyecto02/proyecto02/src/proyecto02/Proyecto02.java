a22b7f8f75a295639a1fba250699463fa8393bad
jdguerrero28 <jdiegogueal@gmail.com>
2025-10-25 00:25:01 -0500

    ejercicio02

---
+++ proyecto02/proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author jdieg
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Nombre de la Carrera:");
+        System.out.println("Computacion");
+        System.out.println("La carrera tiene 9 ciclos academicos");
+        // TODO code application logic here
+    }
+    
+}
