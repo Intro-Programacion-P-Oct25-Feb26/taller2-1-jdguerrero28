d1ab8c037fd379cc4d0ca9ce8603e675a1d31e51
jdguerrero28 <jdiegogueal@gmail.com>
2025-10-25 00:20:49 -0500

    ejercicio01

---
+++ proyecto01/proyecto01/src/proyecto01/Proyecto01.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto01;
+
+/**
+ *
+ * @author jdieg
+ */
+public class Proyecto01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Direccion UTPL:");
+        System.out.println("Calle Marcelino Champagnat s/n, Barrio San Cayetano Alto, Loja, Ecuador");
+        // TODO code application logic here
+    }
+    
+}
