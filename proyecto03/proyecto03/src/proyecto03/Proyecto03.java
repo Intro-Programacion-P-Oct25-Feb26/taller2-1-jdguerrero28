e5cd11688d3b7f578ee540801a6e44bedc6f85c5
jdguerrero28 <jdiegogueal@gmail.com>
2025-10-25 00:29:19 -0500

    ejercicio03

---
+++ proyecto03/proyecto03/src/proyecto03/Proyecto03.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto03;
+
+/**
+ *
+ * @author jdieg
+ */
+public class Proyecto03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("IDE de programacion:");
+        System.out.println("Apache NetBeans");
+        System.out.println("Lenguajes de programacion a usar durante el ciclo:");
+        System.out.println("Python y Java");
+        // TODO code application logic here
+    }
+    
+}
